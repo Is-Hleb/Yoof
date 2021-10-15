d1aa11f2202faa44148b40f49ad8874f1ee9747a
admin <hlebIshenko@gmail.com>
2021-10-15 16:03:57 +0300

    Подключение Страницы ЛК покупателя

--- yoof-back/resources/frontend/app/src/App.vue
+++ yoof-back/resources/frontend/app/src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+<!--    <div id="nav">-->
+<!--      <router-link to="/">Home</router-link> |-->
+<!--      <router-link to="/about">About</router-link>-->
+<!--    </div>-->
+
+<!--      <buyer-cabinet></buyer-cabinet>-->
+
     <router-view/>
   </div>
 </template>
+
 
 <style>
 #app {
